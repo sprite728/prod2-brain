7144f2ab79565444b352b536ce4d38bdc5a8479d
sprite728 <cchuang@server>
2012-07-25 19:54:34 -0400

    Initial commit

---
+++ prod2-brain/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prod2Brain::Application.config.secret_token = 'c20e03ae06c28b5aa8c1f5f7027b4c0e3bc97c3495d4480ed978d4d6c68710f6104fb3f1f7f73a5beb05f66fedfdaaec9eea3d60d410dba87f52235c57102378'
